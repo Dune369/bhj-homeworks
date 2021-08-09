059e221a72ede6be3f860c002f5c8c3a18665ac1
Dinar <sagitoff.dinar@yandex.ru>
2021-08-09 21:14:33 +0300

    Игра-кликер

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let clickerCounter = document.getElementById("clicker__counter"); // счетчик
+let cookie = document.getElementById("cookie"); // картинка
+
+let clicData = cookie.onclick = function() {
+    let clicImg = clickerCounter.textContent = parseFloat(clickerCounter.textContent) + 1;
+    if ( clicImg % 2 == 0) {
+        cookie.width = 200;
+    } else {
+        cookie.width = 180;
+     }
+}
+
+
