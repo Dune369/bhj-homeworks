ae8fd8b1eaad44d5e704c469e8f3629f7671783c
Dinar <sagitoff.dinar@yandex.ru>
2021-12-12 14:32:15 +0300

    Таймер обратного отчета

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -16,4 +16,26 @@ let countFuncDown = function () {
 let timerValue = setInterval(countFuncDown, 1000);
 
 
-// Повышенный уровень сложности #1
+// Повышенный уровень сложности #1 
+let time = 3600;
+
+let сountdownTimer = document.getElementById("сountdownTimer");
+
+function reverseScore() {
+  let hours = Math.floor((time / 60) / 6);
+  let minutes = Math.floor((time / 60));
+  let seconds = time % 60;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  сountdownTimer.innerHTML = `${hours}:${minutes}:${seconds}`;
+ 
+  time--;
+
+  if(time < 0){
+     clearInterval(id)
+  }
+
+}
+
+let id = setInterval(reverseScore, 1000);
