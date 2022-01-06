769b6a951ef223ad1f72437da6c29eaf66d15aef
Dinar <sagitoff.dinar@yandex.ru>
2022-01-07 00:18:42 +0300

    Попап

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,14 @@
 "use strict"
-const modalMain = document.querySelector('#modal_main');
+const modalMain = document.getElementById('modal_main');
 const showSuccess = document.querySelector('.show-success')
-const modalSuccess = document.querySelector('#modal_success');
+const modalSuccess = document.getElementById('modal_success');
 
 
 const modalClose = document.querySelectorAll('.modal__close')
 const divyArray = Array.from(modalClose)
 
 divyArray.filter((element) => !element.classList.contains('show-success'))
-    .forEach((element) => {
+    .forEach( (element) => {
         element.onclick = function() {
             modalMain.classList.remove("modal_active")
             modalSuccess.classList.remove("modal_active")
